48cea42fdc75b445fd7784c296f06699d8f9de45
Rohit Saini <rohitsaini0444@gmail.com>
2026-05-08 13:40:52 +0530

    Updates

---
+++ Grocery.7ca04bae.js
@@ -0,0 +1,2 @@
+var e=globalThis.parcelRequireb877;(0,e.register)("loR3J",function(r,i){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>o,set:void 0,enumerable:!0,configurable:!0});var l=e("ayMG0"),o=()=>(0,l.jsxs)(l.Fragment,{children:[(0,l.jsx)("h1",{children:"Grocery Page it will have all the grocery items"}),(0,l.jsx)("h2",{children:"Coming Soon..."}),(0,l.jsx)("h3",{children:"for now we are just using it for lazy loading"})]})});
+//# sourceMappingURL=Grocery.7ca04bae.js.map
